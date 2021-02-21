d7d45f5d951a9fbc68663a3882b07a83437a8f86
cyrxdzj <63708985+cyrxdzj@users.noreply.github.com>
2021-02-21 19:45:06 +0800

    Update about-card.js

--- src/about-card.js
+++ src/about-card.js
@@ -1,1 +1,40 @@
+const axios = require("axios");
+const {
+  Card,
+  renderError,
+  renderChart,
+  renderNameTitle,
+} = require("./common.js");
 
+/**
+ * 
+ * @param {number} id 用户id
+ * @returns {Object} 获取的用户数据 {name, color, ccfLevel, slogan, followerCount, followingCount}
+ */
+async function fetchStats(id) {
+  //debug 测试请求
+  const res = await axios.get(`https://tc-0glpuj1k4e75e5ec-1300876583.ap-shanghai.service.tcloudbase.com/luogu?id=${id}`);
+
+  //const res = await axios.get(`https://www.luogu.com.cn/user/${id}?_contentOnly`)
+
+  const stats = {
+    name: "NULL",
+    color: "Gray",
+    ccfLevel: 0,
+    slogan: "",
+    followerCount: 0,
+    followingCount:0
+  }
+  if(res.data.code !== 200) {
+    return stats;
+  }
+  const user = res.data.currentData.user;
+  
+  stats.name = user.name;
+  stats.color = user.color;
+  stats.ccfLevel = user.ccfLevel;
+  stats.slogan = user.slogan;
+  stats.followerCount = user.followerCount;
+  stats.followingCount = user.followingCount;
+  
+  
