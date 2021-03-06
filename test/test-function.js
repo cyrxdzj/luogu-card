4e1f4d5129b4da08fcf7985d1d14d75ec720c2f8
cyrxdzj <63708985+cyrxdzj@users.noreply.github.com>
2021-03-06 20:34:37 +0800

    Create test-function.js

---
+++ test/test-function.js
@@ -0,0 +1,19 @@
+const {
+  renderNameTitle,
+  renderAboutText,
+} = require("../src/common.js");
+module.exports = async (req, res) => {
+  res.setHeader("Content-Type", "image/svg+xml");
+  if(req.query.function=="rat")
+  {
+    return res.send("<svg>"+renderAboutText(req.query.followerCount,req.query.followingCount,req.query.slogan)+"</svg>");
+  }
+  else if(req.query.function=="rnt")
+  {
+    return res.send("<svg>"+renderNameTitle(req.query.name,req.query.color,parseInt(req.query.ccfLevel),req.query.title,parseInt(req.query.cardWidth),req.query.title)+"</svg>");
+  }
+  else
+  {
+    return res.send("<svg>"+renderAboutText(3,3,"Hello World!")+"</svg>");
+  }
+}
