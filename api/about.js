a1ce9999cd24377f3092e1a077c8727953c6c9a0
cyrxdzj <63708985+cyrxdzj@users.noreply.github.com>
2021-03-07 20:23:48 +0800

    Create about.js

--- api/about.js
+++ api/about.js
@@ -1,36 +1,31 @@
-const { fetchAbout } = require("../src/about-card.js");
-const { fetchStats } = require("../src/stats-card");
+const { fetchAbout, renderSVG } = require("../src/about-card.js");
 const { renderError } = require("../src/common.js")
 
 module.exports = async (req, res) => {
-  const { id, scores, hide_title, dark_mode, card_width = 500 } = req.query;
+  const { 
+    id, 
+    hide_title, 
+    dark_mode,
+    card_width = 500,
+  } = req.query;
 
   res.setHeader("Content-Type", "image/svg+xml");
   res.setHeader("Cache-Control", "public, max-age=43200"); // 43200s（12h） cache
 
-  const regNum = /^[1-9]\d*$/;
+  const validId = /^[1-9]\d*$/;
   const clamp = (min, max, n) => Math.max(min, Math.min(max, n));
 
-  if (!regNum.test(card_width)) {
-    return res.send(
-      renderError(`卡片宽度"${card_width}"不合法`, { darkMode: dark_mode })
-    );
-  }
-  if(id != undefined && !regNum.test(id)) {
+  if(!validId.test(id)) {
     return res.send(renderError(`"${id}"不是一个合法uid`, {darkMode: dark_mode}));
   }
-
-  let stats = null;
-
-  if(id != undefined) {
-    stats = await fetchStats(id);
+  if(!validId.test(card_width)) {
+    return res.send(renderError(`卡片宽度"${card_width}"不合法`, {darkMode: dark_mode}));
   }
-
-  return res.send(
-    fetchAbout(stats, scores, {
-      hideTitle: stats === null ? true : hide_title,
-      darkMode: dark_mode,
-      cardWidth: clamp(500, 1920, card_width),
-    })
-  );
+  
+  const about = await fetchAbout(id);
+  return res.send(renderSVG(about, {
+    hideTitle: hide_title,
+    darkMode: dark_mode,
+    cardWidth: clamp(500, 1920, card_width),
+  }));
 };
