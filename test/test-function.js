a4201697ee1ef45ac2122b297d01002347e5eee8
cyrxdzj <63708985+cyrxdzj@users.noreply.github.com>
2021-03-16 19:06:41 +0800

    Update test-function.js

--- test/test-function.js
+++ test/test-function.js
@@ -6,14 +6,14 @@ module.exports = async (req, res) => {
   res.setHeader("Content-Type", "image/svg+xml");
   if(req.query.function=="rat")
   {
-    return res.send("<svg>"+renderAboutText(req.query.followerCount,req.query.followingCount,req.query.slogan)+"</svg>");
+    return res.send("<svg>"+renderAboutText(req.query.userType,req.query.followerCount,req.query.followingCount,req.query.ranking,req.query.slogan,req.query.darkmode)+"</svg>");
   }
   else if(req.query.function=="rnt")
   {
-    return res.send("<svg>"+renderNameTitle(req.query.name,req.query.color,parseInt(req.query.ccfLevel),req.query.title,parseInt(req.query.cardWidth),req.query.rightTop)+"</svg>");
+    return res.send("<svg>"+renderNameTitle(req.query.name,req.query.color,parseInt(req.query.ccfLevel),req.query.title,parseInt(req.query.cardWidth),req.query.rightTop,req.query.tag)+"</svg>");
   }
   else
   {
-    return res.send("<svg>"+renderAboutText(3,3,"Hello World!")+"</svg>");
+    return res.send("<svg>"+renderAboutText("普通用户",3,3,1,"Hello World!",false)+"</svg>");
   }
 }
