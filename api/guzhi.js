5a28fea8b92250e46777ed07804b292cccf08c3f
cyrxdzj <63708985+cyrxdzj@users.noreply.github.com>
2021-04-10 13:28:40 +0800

    Create guzhi.js

--- api/guzhi.js
+++ api/guzhi.js
@@ -1,36 +1,50 @@
-const { renderGuzhiCard } = require("../src/guzhi-card.js");
+const { fetchGuzhi, renderGuzhiCard } = require("../src/guzhi-card.js");
 const { fetchAbout } = require("../src/about-card.js");
 const { renderError } = require("../src/common.js")
 
 module.exports = async (req, res) => {
-  const { id, scores, hide_title, dark_mode, card_width = 500 } = req.query;
+    const { id, scores, hide_title, dark_mode, card_width = 500 } = req.query;
+    var finally_scores
 
-  res.setHeader("Content-Type", "image/svg+xml");
-  res.setHeader("Cache-Control", "public, max-age=43200"); // 43200s（12h） cache
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("Cache-Control", "public, max-age=43200"); // 43200s（12h） cache
 
-  const regNum = /^[1-9]\d*$/;
-  const clamp = (min, max, n) => Math.max(min, Math.min(max, n));
+    const regNum = /^[1-9]\d*$/;
+    const clamp = (min, max, n) => Math.max(min, Math.min(max, n));
 
-  if (!regNum.test(card_width)) {
+    if (!regNum.test(card_width)) {
+        return res.send(
+            renderError(`卡片宽度"${card_width}"不合法`, { darkMode: dark_mode })
+        );
+    }
+    if(id != undefined && !regNum.test(id)) {
+        return res.send(renderError(`"${id}"不是一个合法uid`, {darkMode: dark_mode}));
+    }
+
+    let about = null;
+
+    if(id != undefined) {
+        about = await fetchAbout(id);
+    }
+    
+    if(about.ranking>=1&&about.ranking<=1000)
+    {
+        finally_scores=fetchGuzhi(id, about.ranking);
+        if(finally_scores=="Not found.")
+        {
+            finally_scores=scores;
+        }
+    }
+    else
+    {
+        finally_scores=scores;
+    }
+
     return res.send(
-      renderError(`卡片宽度"${card_width}"不合法`, { darkMode: dark_mode })
+        renderGuzhiCard(about, finally_scores, {
+            hideTitle: about === null ? true : hide_title,
+            darkMode: dark_mode,
+            cardWidth: clamp(500, 1920, card_width),
+        })
     );
-  }
-  if(id != undefined && !regNum.test(id)) {
-    return res.send(renderError(`"${id}"不是一个合法uid`, {darkMode: dark_mode}));
-  }
-
-  let about = null;
-
-  if(id != undefined) {
-    about = await fetchAbout(id);
-  }
-
-  return res.send(
-    renderGuzhiCard(about, scores, {
-      hideTitle: about === null ? true : hide_title,
-      darkMode: dark_mode,
-      cardWidth: clamp(500, 1920, card_width),
-    })
-  );
 };
