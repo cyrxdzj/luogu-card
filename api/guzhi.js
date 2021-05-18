1c55e3fbe8d62769fe5d74e81d78fbf8e53e5fa0
cyrxdzj <63708985+cyrxdzj@users.noreply.github.com>
2021-05-18 22:40:55 +0800

    Update guzhi.js

--- api/guzhi.js
+++ api/guzhi.js
@@ -1,6 +1,27 @@
-const { fetchGuzhi, renderGuzhiCard } = require("../src/guzhi-card.js");
+const { renderGuzhiCard } = require("../src/guzhi-card.js");
 const { fetchAbout } = require("../src/about-card.js");
 const { renderError } = require("../src/common.js")
+const axios = require("axios");
+
+async function fetchGuzhi(id, ranking) {
+    var page=ranking/50+(ranking%50==0?1:0);
+    var index=(ranking-1)%50;
+    
+    const res = await axios.get(`https://www.luogu.com.cn/ranking?page=${page}&_contentOnly`);
+    
+    if(res.data.code !== 200) {
+        return "Not found.";
+    }
+    
+    const rankList=res.data.currentData.rankList.result;
+    
+    if(rankList[index].user.uid!=id)
+    {
+        return "Not found.";
+    }
+    
+    return "${rankList[index].basicRating},${rankList[index].socialRating},${rankList[index].contestRating},${rankList[index].practiceRating},${rankList[index].prizeRating}"
+}
 
 module.exports = async (req, res) => {
     const { id, scores, hide_title, dark_mode, card_width = 500 } = req.query;
